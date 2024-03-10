b4aa50ecafc5a1316fdaa98f05e2a1e45ea32901
Mathanraj <madhanmani0601@gmail.com>
2024-03-11 03:23:18 +0530

    updated usagesite.cs

--- JsonSchema.GSoC2024.CorvusQualification/JsonSchema.GSoC2024.CorvusQualification/UsageSite.cs
+++ JsonSchema.GSoC2024.CorvusQualification/JsonSchema.GSoC2024.CorvusQualification/UsageSite.cs
@@ -1,15 +1,12 @@
-﻿
-
-
-using JsonSchema.GSoC2024.ExistingPackage;
+﻿using JsonSchema.GSoC2024.ExistingPackage;
 
 namespace JsonSchema.GSoC2024.CorvusQualification;
 
 public class UsageSite
 {
-	public void DoTheThing()
-	{
-		var provider = new ServiceProvider();
-		provider.PerformService();
-	}
+    public void DoTheThing()
+    {
+        var provider = new ServiceProvider();
+        provider.PerformService();
+    }
 }
